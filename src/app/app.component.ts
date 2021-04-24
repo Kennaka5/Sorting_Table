47e1f8e6414406504009a754fed639fdd9aa1965
Ken Nakayama <kennaka5@gmail.com>
2021-04-24 12:55:32 -0700

    added Rxjs, rendered list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { RestService } from './rest.service';
 import {Tasks} from './tasks';
 @Component({
@@ -6,20 +6,59 @@ import {Tasks} from './tasks';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'SortingTable';
  constructor(private rs: RestService) {}
 
  tasks: Tasks[] = [];
 
- ngOnInit(): void {
-   this.rs.getTasks().subscribe(
-     (response) => {
-       this.tasks = response;
-       console.log(this.tasks)
-     }
-   )
- }
+//  loading: boolean = false;
+//  errorMessage;
+
+ ngOnInit():void {
+   console.log('oninit')
+  this.rs.getTasks().subscribe(
+    (response) => {
+      this.tasks = response;
+      console.log(this.tasks)
+    }
+  )
+}
 
 }
+
+
+
+
+
+// ngOnInit() {
+//   this.rs.getTasks().subscribe(
+//     (response) => {
+//       this.tasks = response;
+//       console.log(this.tasks)
+//     }
+//   )
+// }
+
+/**
+ *  public getTasks() {
+  this.loading = true;
+  this.errorMessage = "";
+  this.rs.getTasks()
+    .subscribe(
+      (response) => {                           //next() callback
+        console.log('response received')
+        this.tasks = response; 
+      },
+      (error) => {                              //error() callback
+        console.error('Request failed with error')
+        this.errorMessage = error;
+        this.loading = false;
+      },
+      () => {                                   //complete() callback
+        console.error('Request completed')      //This is actually not needed 
+        this.loading = false; 
+      })
+}
+ */
 
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError} from 'rxjs/operators';
+
 import { Tasks } from './tasks';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class RestService {
 
   constructor(private http : HttpClient) { }
 
+  // TODO: move this URL into environment config file
+  // https://balramchavan.medium.com/configure-and-build-angular-application-for-different-environments-7e94a3c0af23
     url : string = 'http://localhost:3000/tasks'
 
-    getTasks() {
+    getTasks(): Observable<Tasks[]> {
       return this.http.get<Tasks[]>(this.url)
     }
 
